5fb392065c61196bd974382feb1222c1c29916d7
GQiangQ <740407980@qq.com>
2020-11-28 21:37:27 +0800

    完成操作

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const inquirer = require('inquirer')
+
 const homedir =process.env.HOME || require('os').homedir()
 const path = require('path')
 
@@ -12,12 +13,71 @@ module.exports.add = async (title) => {
 }
  
 module.exports.clear = async() => {
-  await db.write(dbPath, '[]')
+  await db.write('[]')
 }
 module.exports.look = async() => {
   let list = await db.read()
-  console.log('任务列表：\n')
-  list.forEach((element,index) => {
-    console.log(`${index+1}  __ ${element.title}`)
-  });
+  inquirer.prompt([
+    {
+      type:'list',
+      name:'index',
+      message:'请选择你的任务:',
+      choices: [...list.map((item,index) =>{
+        return {name:`${ item.done ? '[√]' : '[×]' } ${index+1} - ${item.title}`,value:index}
+      }),{name:'退出', value:-1},{name:'添加', value:-2}]
+    }
+  ]).then(res => {
+    const index = res.index
+    if(res.index >= 0){
+      inquirer.prompt([
+        {
+          type:'list',
+          name:'action',
+          message:'请选择你的操作:',
+          choices: [
+            {name:'完成', value:1},
+            {name:'未完成', value:2},
+            {name:'改标题', value:3},
+            {name:'删除', value:4},
+            {name:'退出', value:-1},
+          ],
+        }]).then(res=>{
+          const action = res.action
+          if(action === 1 ) {
+            list[index].done = true
+            db.write(JSON.stringify(list))
+          }
+          if(action === 2 ) {
+            list[index].done = false
+            db.write(JSON.stringify(list))
+          }
+          if(action === 3 ) {
+            inquirer.prompt({
+              type: 'input',
+              name: 'title',
+              message: '输入修改的标题:'
+            }).then(res=>{
+              const title = res.title
+              list[index].title = title
+              db.write(JSON.stringify(list))
+            })
+          }
+          if(action === 4 ) {
+            list.splice(index,1)
+            db.write(JSON.stringify(list))
+          }
+        })
+    }
+    if (index === -2) {
+      inquirer.prompt({
+        type: 'input',
+        name: 'title',
+        message: '输入添加的任务:'
+      }).then(res=>{
+        const title = res.title
+        list.push({title,done: false})
+        db.write(JSON.stringify(list))
+      })
+    }
+  })
 }
